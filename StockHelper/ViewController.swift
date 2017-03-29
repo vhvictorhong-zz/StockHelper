a16c848e78d888799844032875346cdcd1800200
victorhong1988 <victorhong1988@gmail.com>
2017-03-28 22:53:28 -0400

    Initial Commit

---
+++ StockHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockHelper
+//
+//  Created by Victor Hong on 28/03/2017.
+//  Copyright © 2017 Victor Hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
