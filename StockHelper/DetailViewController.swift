e84435bcb3c8577eeb0f296b0da0a72a8e4c3926
victorhong1988 <victorhong1988@gmail.com>
2017-07-27 23:08:18 -0400

    Add Target Tab

--- StockHelper/DetailViewController.swift
+++ StockHelper/DetailViewController.swift
@@ -23,7 +23,6 @@ class DetailViewController: UIViewController {
         navigationItem.title = stockSymbol
         let btn1 = UIButton(type: UIButtonType.contactAdd)
         btn1.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
-        
         btn1.addTarget(self, action: #selector(DetailViewController.addToList), for: .touchUpInside)
         let item1 = UIBarButtonItem(customView: btn1)
         
