7aabb61d59e35a50345e3df287381ea518044337
victorhong1988 <victorhong1988@gmail.com>
2017-07-28 20:09:26 -0400

    Setup TargetCollectionViewCell

---
+++ StockHelper/TargetCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  TargetCollectionViewCell.swift
+//  StockHelper
+//
+//  Created by Victor Hong on 28/07/2017.
+//  Copyright © 2017 Victor Hong. All rights reserved.
+//
+
+import UIKit
+
+class TargetCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
